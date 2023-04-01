6f2633d3b8291d63550145023e87ea4dc6d46fd0
Alan Ramalho <ramalho.sit@gmail.com>
2023-04-01 17:18:41 -0300

    order!

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,9 @@ import cors from 'cors';
 import { routes } from './routes';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use(cors());
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
   if(err instanceof Error) {
