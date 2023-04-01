88d77dab57decb6f19ebf333161ef8337b7cd33e
Alan Ramalho <ramalho.sit@gmail.com>
2023-04-01 13:58:33 -0300

    token middleware.

--- src/modules/deliveries/useCases/createDelivery/CreateDeliveryController.ts
+++ src/modules/deliveries/useCases/createDelivery/CreateDeliveryController.ts
@@ -5,12 +5,12 @@ import { CreateDeliveryUseCase } from "./CreateDeliveryUseCase";
 
 export class CreateDeliveryController {
   async handle(request: Request, response: Response) {
-    const { item_name, id_client } = request.body;
+    const { item_name } = request.body;
     const createDeliveryUseCase = new CreateDeliveryUseCase();
     try {
       const result = await createDeliveryUseCase.execute({
         item_name,
-        id_client
+        id_client: +request.id_client,
       });
       return response.json(result);
     } catch (error) {
--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { AuthenticateClientController } from './modules/account/authenticateClie
 import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController';
 import { AuthenticateDeliverymanController } from './modules/account/authenticateDeliveryman/AuthenticateDeliverymanController';
 import { CreateDeliveryController } from './modules/deliveries/useCases/createDelivery/CreateDeliveryController';
+import { ensureAuthenticateClient } from './middlewares/ensureAuthenticateClient';
 
 const routes = Router();
 
@@ -20,6 +21,6 @@ const createDeliverymanController = new CreateDeliverymanController();
 routes.post('/deliveryman/', createDeliverymanController.handle);
 
 const createDeliveryController = new CreateDeliveryController();
-routes.post('/delivery', createDeliveryController.handle);
+routes.post('/delivery', ensureAuthenticateClient, createDeliveryController.handle);
 
 export { routes }
