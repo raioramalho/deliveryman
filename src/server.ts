c31f3b04f58873e86e6169ca1ac3a116a06d909e
Alan Ramalho <ramalho.sit@gmail.com>
2023-04-01 17:18:10 -0300

    enable cors.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 import { routes } from './routes';
 
 const app = express();
 app.use(express.json());
 app.use(routes);
+app.use(cors());
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
   if(err instanceof Error) {
